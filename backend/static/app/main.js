ace79d6feb5883a5181c34bd9872ce10534b68e3
Gorkem Tosun <gorkemtosun@gmail.com>
2019-11-23 20:42:16 +0100

    involve frontend to backend

---
+++ backend/static/app/main.js
@@ -0,0 +1,358 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNjZiOGU0OC05ODlmLTRjMWEtYjZlMC04MThkYzMzZDk1NWIiLCJpZCI6MTQ5MjEsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjY3MzgzMDZ9.mAhC5QxwAO-yOTMialgM-Wha7NVRe-XIF7bOijyJCsI'
+var viewer = new Cesium.Viewer('cesiumContainer');
+var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+    url: 'http://localhost:3000/static/Converteritu/tileset.json', // URL from `Starting the Server` section.
+}));
+// console.log(tileset.modelMatrix);
+var position = new Cesium.Cartesian3(
+    4208684.741335429,
+    2334783.82860767,
+    4171228.132652792
+)
+// var edgesPosition3 = new Cesium.Cartesian3(
+//     -126.360932888,
+//     22.6368887922,
+//     114.059850757
+// )
+// var edgesPosition = new Cesium.Cartesian3(
+//     4208811.102268317,
+//     2334761.1917188778,
+//     4171112.072802035,
+// )
+// var edgesPosition2 = new Cesium.Cartesian3(
+//     1,
+//     1,
+//     1,
+// )
+
+tileset.modelMatrix = {
+    0: 0.560397193631008,
+    1: -0.8215581379773444,
+    2: -0.10486758933859872,
+    3: 0,
+    4: 0.5017949269900436,
+    5: 0.43752678784028526,
+    6: -0.7461716700392871,
+    7: 0,
+    8: 0.6589057873607934,
+    9: 0.365530485521195,
+    10: 0.6574424899085031,
+    11: 0,
+    12: 4208684.741335429,
+    13: 2334783.82860767,
+    14: 4171228.132652792,
+    15: 1
+}
+var edgesModelMatrix = {
+    0: -0.5017949269900434,
+    1: -0.4375267878402857,
+    2: 0.7461716700392871,
+    3: 0,
+    4: 0.5603971936310084,
+    5: -0.8215581379773442,
+    6: -0.10486758933859909,
+    7: 0,
+    8: 0.6589057873607935,
+    9: 0.36553048552119505,
+    10: 0.6574424899085032,
+    11: 0,
+    12: 4208684.741335429,
+    13: 2334783.82860767,
+    14: 4171228.132652792,
+    15: 1
+}
+
+viewer.scene.globe.ellipsoid = Cesium.Ellipsoid.WGS84
+viewer.extend(Cesium.viewerCesiumInspectorMixin);
+var scene = viewer.scene
+// On mouse over, display all the properties for a feature in the console log.
+var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas)
+handler.setInputAction(function (click) {
+    var feature = scene.pick(click.position);
+    if (feature instanceof Cesium.Cesium3DTileFeature) {
+        console.log(feature)
+        feature.color = Cesium.Color.fromAlpha(Cesium.Color.GRAY, 0.5);
+        var propertyNames = feature.getPropertyNames();
+        var length = propertyNames.length;
+        for (var i = 0; i < length; ++i) {
+            var propertyName = propertyNames[i];
+            // console.log(propertyName + ': ' + feature.getProperty(propertyName));
+        }
+    }
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+
+// click over the globe to see the cartographic position
+var coordinateClickHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+coordinateClickHandler.setInputAction(function (movement) {
+    var cartesian = viewer.camera.pickEllipsoid(movement.position, scene.globe.ellipsoid);
+    // console.log(cartesian)
+    var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+    var longitudeString = Cesium.Math.toDegrees(cartographic.longitude);
+    var latitudeString = Cesium.Math.toDegrees(cartographic.latitude);
+    // console.log(longitudeString, latitudeString)
+    // viewer.zoomTo(tileset)
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+var queryPath;
+function getRoute() {
+    console.log(queryPath)
+    if (queryPath) {
+        scene.primitives.remove(queryPath)
+    }
+    console.log('Getting the route')
+    var from = document.getElementById("from").value
+    var to = document.getElementById("to").value
+    axios.get(`http://localhost:3000/path/${from}/${to}?wheelchair=False`).then(res => {
+        queryPath = new Cesium.PolylineCollection()
+        res.data.features.forEach(feature => {
+            var coordinatesArray = []
+            feature.geometry.coordinates.forEach(coordinatePair => {
+                var vertex = new Cesium.Cartesian3(
+                    coordinatePair[0],
+                    coordinatePair[1],
+                    coordinatePair[2]
+                )
+                coordinatesArray.push(vertex)
+                queryPath.add({
+                    positions: coordinatesArray,
+                    width: 5,
+                    material: new Cesium.Material({
+                        fabric: {
+                            type: 'Color',
+                            uniforms: {
+                                color: Cesium.Color.RED
+                            }
+                        }
+                    })
+                })
+            })
+        })
+        queryPath.modelMatrix = edgesModelMatrix
+        scene.primitives.add(queryPath)
+    })
+}
+
+
+var edges;
+// EDGES
+axios.get('http://localhost:3000/edges').then(res => {
+    // console.log(res.data)
+    edges = new Cesium.PolylineCollection()
+    res.data.features.forEach(feature => {
+        var coordinatesArray = []
+        feature.geometry.coordinates.forEach(coordinatePair => {
+            // console.log(coordinatePair)
+            var vertex = new Cesium.Cartesian3(
+                coordinatePair[0],
+                coordinatePair[1],
+                coordinatePair[2]
+            )
+            coordinatesArray.push(vertex)
+            edges.add({
+                positions: coordinatesArray,
+                width: 1,
+                material: new Cesium.Material({
+                    fabric: {
+                        type: 'Color',
+                        uniforms: {
+                            color: new Cesium.Color(1.0, 1.0, 0.0, 1.0)
+                        }
+                    }
+                })
+            })
+        })
+    })
+    edges.modelMatrix = edgesModelMatrix
+    scene.primitives.add(edges)
+})
+
+var rooms;
+// ROOMS
+axios.get('http://localhost:3000/rooms').then(res => {
+    // console.log(res.data)
+    var polygonInstances = []
+    res.data.features.forEach(feature => {
+        var coordinatesArray = []
+        feature.geometry.coordinates[0].forEach(coordinatePair => {
+            // console.log(coordinatePair)
+            var vertex = new Cesium.Cartesian3.fromArray(coordinatePair)
+            coordinatesArray.push(vertex)
+        })
+        // var polygon = 
+        // var geometry = Cesium.PolygonGeometry.createGeometry(polygon);
+        var geometryInstance = new Cesium.GeometryInstance({
+            // geometry: Cesium.PolygonGeometry.fromPositions({
+            //     positions: coordinatesArray,
+            //     attributes: {
+            //         arcType: Cesium.ArcType.None,
+            //         height: 500
+            //     }
+            // })
+            geometry: Cesium.PolygonGeometry.createGeometry(new Cesium.PolygonGeometry({
+                polygonHierarchy: new Cesium.PolygonHierarchy(coordinatesArray),
+                attributes: {
+                    arcType: Cesium.ArcType.NONE
+                }
+            })),
+            attributes: {
+                arcType: Cesium.ArcType.NONE
+            }
+            // attributes: {
+            //     color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.AQUA)
+            // }
+        })
+        polygonInstances.push(geometryInstance)
+    })
+    rooms = new Cesium.Primitive({
+
+        geometryInstances: polygonInstances,
+        appearance: new Cesium.MaterialAppearance({
+            material: Cesium.Material.fromType('Color'),
+            faceForward: true
+        }),
+        // modelMatrix: edgesModelMatrix,
+        asynchronous: false,
+        releaseGeometryInstances: false
+    })
+    // appearance: new Cesium.MaterialAppearance({
+    //     material: Cesium.Material.fromType('Color'),
+    //     faceForward: true
+    // })
+    // new Cesium.Material({
+    //     fabric: {
+    //         type: 'Color',
+    //         uniforms: {
+    //             color: new Cesium.Color(1.0, 1.0, 0.0, 1.0)
+    //         }
+    //     }
+    // }),
+    scene.primitives.add(rooms)
+})
+
+viewer.zoomTo(tileset);
+
+
+// var heading_angle = 0
+// var pitch_angle = 0
+// var roll_angle = 0
+// var hpr = new Cesium.HeadingPitchRoll(
+//     Cesium.Math.toRadians(heading_angle),
+//     Cesium.Math.toRadians(pitch_angle),
+//     Cesium.Math.toRadians(roll_angle)
+// );
+// // console.log(hpr);
+// var orientation = Cesium.Transforms.headingPitchRollQuaternion(edgesPosition, hpr);
+// var rotationMatrix = Cesium.Matrix3.fromQuaternion(orientation)
+// Cesium.Matrix4.multiplyByMatrix3(edges.modelMatrix, rotationMatrix, edges.modelMatrix)
+
+function rotate(heading_angle, pitch_angle, roll_angle) {
+    // Rotate an object based on a position
+    var hpr = new Cesium.HeadingPitchRoll(
+        Cesium.Math.toRadians(heading_angle),
+        Cesium.Math.toRadians(pitch_angle),
+        Cesium.Math.toRadians(roll_angle)
+    );
+    Cesium.Transforms.headingPitchRollToFixedFrame(
+        origin = position,
+        headingPitchRoll = hpr,
+        ellipsoid = Cesium.Ellipsoid.WGS84,
+        fixedFrameTransform = Cesium.Transforms.eastNorthUpToFixedFrame,
+        result = rooms.modelMatrix
+    )
+}
+
+var heading = 0
+var pitch = 0
+var roll = 0
+var precision = 1
+window.onkeydown = function (e) {
+    console.log(e);
+    if (e.key == 'ArrowUp') {
+        heading += precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.key == 'ArrowDown') {
+        heading -= precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.key == 'ArrowLeft') {
+        pitch += precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.key == 'ArrowRight') {
+        pitch -= precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.key == '4') {
+        roll -= precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.key == '6') {
+        roll += precision
+        rotate(heading, pitch, roll)
+    }
+    if (e.code == 'Space') {
+        setInterval(() => {
+            roll -= precision
+            pitch -= precision
+            heading -= precision
+            console.log("dondir")
+            rotate(heading, pitch, roll, tileset)
+            rotate(heading, pitch, roll, edges)
+        }, 100);
+    }
+
+}
+
+
+
+//Create a transform for the offset.
+// var enuTransform = Cesium.Transforms.eastNorthUpToFixedFrame(position);
+
+
+// Adjust a tileset's height from the globe's surface.
+
+// var center = Cesium.Cartesian3(4208713.4652421214, 2334833.6289644390);
+// var angle = Cesium.Quaternion.fromAxisAngle({
+//     x: 1,
+//     y: 0,
+//     z: 0
+// }, Cesium.Math.toRadians(10))
+// console.log(angle)
+// var orientation = new Cesium.TranslationRotationScale()
+// tileset.modelMatrix = Cesium.Matrix4.fromTranslationRotationScale(orientation)
+// viewer.entities.add(tileset)
+// var entity = {
+//     id: 'itu',
+//     label: {
+//         show: true,
+//     },
+//     model: tileset
+// }
+
+// var entity = viewer.entities.getById('itu');
+// console.log(entity)
+
+// viewer.scene.canvas.addEventListener('click', function (e) {
+//     viewer.zoomTo(tileset);
+//     var entity = viewer.entities.getById('mou');
+//     var ellipsoid = viewer.scene.globe.ellipsoid;
+//     // Mouse over the globe to see the cartographic position 
+//     var cartesian = viewer.camera.pickEllipsoid(new Cesium.Cartesian3(e.clientX, e.clientY), ellipsoid);
+//     if (cartesian) {
+//         var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+//         var x = (cartesian.x).toFixed(10);
+//         var y = (cartesian.y).toFixed(10);
+//         var z = (cartesian.z).toFixed(10);
+//         entity.position = cartesian;
+//         entity.label.show = true;
+//         entity.label.font_style = 84;
+//         //entity.position= Cesium.Cartesian2.ZERO; 
+//         entity.label.text = '(' + x + ', ' + y + ', ' + z + ')';
+//         var result = entity.label.text; // we can reuse this
+//         document.getElementById("demo").innerHTML = entity.label.text;
+//     } else {
+//         entity.label.show = false;
+//     }
+// });
--- backend/views/default.py
+++ backend/views/default.py
@@ -5,6 +5,8 @@ from sqlalchemy.exc import DBAPIError
 
 from .. import models
 import json
+
+
 def sqlStringForFeatureCollection(table_name, geom_name="geom"):
     return """SELECT jsonb_build_object(
                 'type',     'FeatureCollection',
@@ -16,7 +18,10 @@ def sqlStringForFeatureCollection(table_name, geom_name="geom"):
                 'properties', to_jsonb(row) - '{geom_name}'
             ) AS feature
                 FROM (SELECT * FROM {table_name}) AS row) AS features 
-        """.format(table_name=table_name, geom_name=geom_name)
+        """.format(
+        table_name=table_name, geom_name=geom_name
+    )
+
 
 def dijkstraString(edge_table, from_pnt, to_pnt, wheelchair=""):
     return """
@@ -26,21 +31,30 @@ def dijkstraString(edge_table, from_pnt, to_pnt, wheelchair=""):
                 {from_pnt}, {to_pnt}
             ) AS result 
             JOIN {edge_table} as f_edges ON result.edge = f_edges._id
-        """.format(edge_table=edge_table, from_pnt=from_pnt, to_pnt=to_pnt, wheelchair=wheelchair)
+        """.format(
+        edge_table=edge_table, from_pnt=from_pnt, to_pnt=to_pnt, wheelchair=wheelchair
+    )
+
 
 @view_config(route_name="rooms", renderer="json")
 def rooms(request):
     table_name = "rooms"
-    #result is a tuple so we need to get the 0 index
-    result = request.dbsession.execute(sqlStringForFeatureCollection(table_name=table_name)).first()[0]  
+    # result is a tuple so we need to get the 0 index
+    result = request.dbsession.execute(
+        sqlStringForFeatureCollection(table_name=table_name)
+    ).first()[0]
     return Response("done", status=200, json=result)
+
 
 @view_config(route_name="edges", renderer="json")
 def edges(request):
     table_name = "edges"
-    #result is a tuple so we need to get the 0 index
-    result = request.dbsession.execute(sqlStringForFeatureCollection(table_name=table_name)).first()[0]  
+    # result is a tuple so we need to get the 0 index
+    result = request.dbsession.execute(
+        sqlStringForFeatureCollection(table_name=table_name)
+    ).first()[0]
     return Response("done", status=200, json=result)
+
 
 @view_config(route_name="path", renderer="json")
 def path(request):
@@ -51,8 +65,11 @@ def path(request):
     if request.params.get("wheelchair"):
         wheelchair = "WHERE wheelchair=true"
     table_name = dijkstraString(edge_table, from_pnt, to_pnt, wheelchair)
-    result = request.dbsession.execute(sqlStringForFeatureCollection(table_name=table_name)).first()[0]
+    result = request.dbsession.execute(
+        sqlStringForFeatureCollection(table_name=table_name)
+    ).first()[0]
     return Response("done", status=200, json=result)
+
 
 @view_config(route_name="nodes", renderer="json")
 def nodes(request):
@@ -61,9 +78,12 @@ def nodes(request):
         table_name = "first_floor_edges_vertices_pgr"
     elif floor == "2nd":
         table_name = "second_floor_edges_vertices_pgr"
-    result = request.dbsession.execute(sqlStringForFeatureCollection(
-        table_name=table_name,
-        geom_name="the_geom"
-        )).first()[0]
+    result = request.dbsession.execute(
+        sqlStringForFeatureCollection(table_name=table_name, geom_name="the_geom")
+    ).first()[0]
     return Response("done", status=200, json=result)
 
+
+@view_config(route_name="home", renderer="templates/home.jinja2")
+def home(request):
+    return {"message": "Hellooo!"}
